150854f6dcb634b742a8309a90acbb02af726ba4
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2022-09-25 02:20:04 +1000

    fixed name for mapnpc (nyi)

--- files/entities/npcs/mapnpc/interact.lua
+++ files/entities/npcs/mapnpc/interact.lua
@@ -6,7 +6,7 @@ local player = EntityGetInRadiusWithTag(x, y, 10, "player_unit")[1]
 
 function interacting( entity_who_interacted, entity_interacted, interactable_name )
     dialog_system.open_dialog( {
-        name = "Tuoli",
+        name = "Sormi",
         portrait = "mods/mould/files/entities/npcs/intronpc/portrait.png",
         typing_sound = "two",
         text = "Hello #Minä#.",
