45b7ddaf3638d64b5dfc8384029e008ee3534e10
Mir Sole <zburn101@gmail.com>
2022-09-25 22:07:10 +1000

    change

--- files/entities/npcs/mapnpc/interact.lua
+++ files/entities/npcs/mapnpc/interact.lua
@@ -15,7 +15,7 @@ function interacting( entity_who_interacted, entity_interacted, interactable_nam
                 text="I was told you had a task for me?",
                 func = function(dialogue)
                     local flag = "objective_intro_maproom"
-                    local objtext = "I need you to retrieve the ~lost part of my map~ from our old village \nbefore we were forced underground. If you go left from the exit to our base \nyou should find the village's location easily."
+                    local objtext = "I need you to retrieve the ~lost part of my map~ from \nour old village before we were forced underground. \nIf you go left from the exit to our base \nyou should find the village's location easily."
                     if GameHasFlagRun(flag) == true then
                         dialogue.show( {
                             text=objtext,
@@ -26,7 +26,7 @@ function interacting( entity_who_interacted, entity_interacted, interactable_nam
                         GameAddFlagRun("DONE_" .. flag)
                     elseif GameHasFlagRun("objective_retrievemap") then
                         dialogue.show( {
-                            text="I have told you this. " .. objtext,
+                            text="I have told you this.\n " .. objtext,
                         } )
                     elseif GameHasFlagRun("DONE_objective_retrievemap") then
                         dialogue.show( {
