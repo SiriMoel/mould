833e487df682ad84a80343c72c07cf4e6b115ed2
Mir Sole <zburn101@gmail.com>
2022-10-05 16:15:06 +1100

    a singular building that took 4 hours

--- files/entities/npcs/intronpc/interact.lua
+++ files/entities/npcs/intronpc/interact.lua
@@ -11,10 +11,10 @@ function interacting( entity_who_interacted, entity_interacted, interactable_nam
         name = "Tuoli",
         portrait = "mods/mould/files/entities/npcs/intronpc/portrait.png",
         typing_sound = "two",
-        text = "Hello ~" .. pname .. "~. Do not worry yourself about the loss of the\n ~Throngler~, atleast the mages do not have it now eh? As for \nthe hearts... I may have a lead on how we can deal with them \nbut thats for future discussion.",
+        text = "Hello ~" .. pname .. "~. Nevermind the failed thievery of the\n ~Throngler~, atleast the mages do not have it now eh? As for \nthe hearts... I may have a lead on how we can deal with them \nbut thats for future discussion.",
         options = {
             {
-                text="What should I be doing?", -- maybe expand this for a large portion of the story?
+                text="What should I be doing now then?", -- maybe expand this for a large portion of the story?
                 func = function(dialogue)
                     local flag = "intro_armoury"
                     local flagtwo = "intro_maproom"
@@ -31,9 +31,6 @@ function interacting( entity_who_interacted, entity_interacted, interactable_nam
                     end
                 end,
             },
-            {
-                text="Ok.",
-            }
         },
     } )
 end
