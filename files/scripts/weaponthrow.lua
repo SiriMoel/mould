d52f9bfed1017a547ade68842c494c4aeb1f1e15
Mir Sole <zburn101@gmail.com>
2022-10-29 21:44:47 +1100

    the change

--- files/scripts/weaponthrow.lua
+++ files/scripts/weaponthrow.lua
@@ -5,7 +5,7 @@ local player = EntityGetWithTag("player_unit")[1]
 local comp_controls = EntityGetFirstComponentIncludingDisabled(player, "ControlsComponent")
 local comp_ability = EntityGetFirstComponentIncludingDisabled(weapon, "AbilityComponent")
 
-if ComponentGetValue2(comp_controls, "mButtonDownThrow") == false then
+if ComponentGetValue2(comp_controls, "mButtonDownThrow") ~= false then
     ComponentSetValue2(comp_ability, "throw_as_item", true)
 else
     ComponentSetValue2(comp_ability, "throw_as_item", false)
